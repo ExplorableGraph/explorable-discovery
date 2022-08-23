2495952ca2b6c3fabfb2969db6e6c714fcf9a556
Jan Miksovsky <jan@miksovsky.com>
2022-08-23 11:00:09 -0700

    Comments

--- src/test/explorable.test.js
+++ src/test/explorable.test.js
@@ -1,9 +1,9 @@
 import assert from "node:assert";
 import test from "node:test";
 
-// Direct Explorable representation of a function and domain
+// Explorable graph representation of a function and a domain
 const graph = {
-  // Iterator that return the keys (domain) in this graph
+  // Iterator returns the keys (domain) in this graph.
   async *[Symbol.asyncIterator]() {
     const domain = ["Alice", "Bob", "Carol"];
     for (const key of domain) {
@@ -11,8 +11,7 @@ const graph = {
     }
   },
 
-  // Function that returns the value for a given key
-  // get("Alice") returns "Hello, Alice!"
+  // Get function returns the value for a given key.
   async get(key) {
     return `Hello, ${key}!`;
   },
