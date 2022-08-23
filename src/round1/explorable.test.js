fb541405a6a7ce03f3a268b577b0ec0ca310e295
Jan Miksovsky <jan@miksovsky.com>
2022-08-23 10:57:18 -0700

    Kata

--- src/round1/explorable.test.js
+++ src/round1/explorable.test.js
@@ -1,21 +1,18 @@
 import assert from "node:assert";
 import test from "node:test";
-import graph from "./explorable.js";
+// import graph from "./explorable.js";
 
 test("can iterate over the keys of the graph", async () => {
   const keys = [];
-  for await (const key of graph) {
-    keys.push(key);
-  }
   assert.deepEqual(keys, ["Alice", "Bob", "Carol"]);
 });
 
 test("can get the value for a key", async () => {
-  const alice = await graph.get("Alice");
+  const alice = undefined;
   assert.equal(alice, "Hello, Alice!");
 });
 
 test("can get the value for a key that is not included by the iterator", async () => {
-  const david = await graph.get("David");
+  const david = undefined;
   assert.equal(david, "Hello, David!");
 });
