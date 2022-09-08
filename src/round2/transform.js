01cebbd272fe03d95b183d5abf6264350233e453
Jan Miksovsky <jan@miksovsky.com>
2022-09-08 10:36:36 -0700

    Intro

--- src/round2/transform.js
+++ src/round2/transform.js
@@ -1,22 +1,9 @@
-import { marked } from "marked";
+// import { marked } from "marked";
 
 export default function transform(graph) {
   return {
-    async *[Symbol.asyncIterator]() {
-      for await (const markdownKey of graph) {
-        const htmlKey = markdownKey.replace(/\.md$/, ".html");
-        yield htmlKey;
-      }
-    },
+    async *[Symbol.asyncIterator]() {},
 
-    async get(htmlKey) {
-      const markdownKey = htmlKey.replace(/\.html$/, ".md");
-      let markdown = await graph.get(markdownKey);
-      if (markdown === undefined) {
-        return undefined;
-      }
-      markdown = markdown.toString();
-      return marked(markdown);
-    },
+    async get(htmlKey) {},
   };
 }
