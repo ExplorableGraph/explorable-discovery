784fa31d85d37891d6d2a0e415b7cb478342e7c0
Jan Miksovsky <jan@miksovsky.com>
2022-09-11 21:43:57 -0700

    Intro

--- src/round2/folder.js
+++ src/round2/folder.js
@@ -3,7 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const moduleFolder = path.dirname(fileURLToPath(import.meta.url));
-const dirname = path.resolve(moduleFolder, "../markdown");
+const dirname = path.resolve(moduleFolder, "../data/folder");
 
 export default {
   async *[Symbol.asyncIterator]() {},
