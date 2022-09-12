e897f1b28f0869402bd188e67c75df0475121898
Jan Miksovsky <jan@miksovsky.com>
2022-09-12 08:41:04 -0700

    Intro

--- src/round1/graph.js
+++ src/round1/graph.js
@@ -1,15 +1,8 @@
 // Explorable graph representation of a function and a domain
 export default {
   // Iterator returns the keys (domain) in this graph.
-  async *[Symbol.asyncIterator]() {
-    yield* ["Alice.md", "Bob.md", "Carol.md"];
-  },
+  async *[Symbol.asyncIterator]() {},
 
   // Get function returns the value for a given key.
-  async get(key) {
-    if (key.endsWith(".md")) {
-      const name = key.replace(/\.md$/, "");
-      return `Hello, **${name}**.`;
-    }
-  },
+  async get(key) {},
 };
